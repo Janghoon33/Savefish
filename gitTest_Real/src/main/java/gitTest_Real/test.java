31f06044926a4fac0fba7c0a3390acf583d2660b
smhrd <smhrd@59.0.236.92>
2022-04-29 12:15:53 +0900

    장훈 업로드

--- gitTest_Real/src/main/java/gitTest_Real/test.java
+++ gitTest_Real/src/main/java/gitTest_Real/test.java
@@ -4,7 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("태린테스느");
+		System.out.println("태린테스느");
+		System.out.println("뭘봐 바보야");
 	}
 
 }
