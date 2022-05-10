eb1d4c788118618e7443570ba069b397e9a91baf
smhrd <smhrd@59.0.236.229>
2022-05-10 15:25:48 +0900

    수정

--- gitTest_Real/src/main/java/gitTest_Real/test.java
+++ gitTest_Real/src/main/java/gitTest_Real/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("태린테스느");
+		System.out.println("태린 바보");
 		System.out.println("뭘봐 바보야");
 	}
 
